08f9d8994a355eee3435af8244007747fc612085
Jon Mills <jonathanfmills@gmail.com>
2018-02-15 06:58:27 -0600

    Adding Title ENV variable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const title = process.env.TITLE;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title } );
 });
 
 module.exports = router;
